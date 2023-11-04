d6068d4871f085b447bd330a20361a69f75013e1
CarloHD <carloborrometi@gmail.com>
2023-11-04 15:01:51 +0100

    correzione environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,48 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Negozio } from './shared/negozio.model';
-import { AuthService } from './service/auth.service';
-import { StoreNegoziService } from './service/store-negozi.service';
-import { Subscription } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { Negozio } from './shared/negozio.model'
+import { AuthService } from './service/auth.service'
+import { StoreNegoziService } from './service/store-negozi.service'
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnDestroy {
-  title = 'Gestionale';
+  title = 'Gestionale'
 
-  listaNegozi: Negozio[];
-  listaNegoziSub: Subscription;
+  listaNegozi: Negozio[]
+  listaNegoziSub: Subscription
 
-  username: string;
-  usernameSub: Subscription;
+  username: string
+  usernameSub: Subscription
 
-  isLogged: boolean;
+  isLogged: boolean
 
-  constructor(
+  constructor (
     private storeNegoziService: StoreNegoziService,
     private authService: AuthService
   ) {}
 
-  ngOnInit(): void {
-    this.usernameSub = this.authService.usernameEmitter.subscribe(
-      (username) => {
-        this.username = username;
-        this.isLogged = !!username;
-      }
-    );
+  ngOnInit (): void {
+    this.usernameSub = this.authService.usernameEmitter.subscribe(username => {
+      this.username = username
+      this.isLogged = !!username
+    })
 
     this.listaNegoziSub = this.storeNegoziService.listaNegoziEmit.subscribe(
-      (arrayNegozi) => (this.listaNegozi = arrayNegozi)
-    );
+      arrayNegozi => (this.listaNegozi = arrayNegozi)
+    )
   }
 
-  logout() {
-    this.authService.onLogout();
+  logout () {
+    this.authService.onLogout()
   }
 
-  ngOnDestroy(): void {
-    this.listaNegoziSub.unsubscribe();
+  ngOnDestroy (): void {
+    this.listaNegoziSub.unsubscribe()
 
-    this.usernameSub.unsubscribe();
+    this.usernameSub.unsubscribe()
   }
 }
